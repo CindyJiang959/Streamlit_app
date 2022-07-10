2e69ec87c6abbc066d324039e67e6f3567389f1e
JoyceJiang <47199404+CindyJiang959@users.noreply.github.com>
2022-07-10 18:30:22 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,4 +21,4 @@ streamlit.dataframe(fruit_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit_text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json())
